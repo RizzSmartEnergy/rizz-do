2b799438da062a95c543e82a85deb36aed062aef
Haidar Ali <haidarali.eu@gmail.com>
2023-06-23 14:27:05 +0700

    change sequences

--- src/DO.h
+++ src/DO.h
@@ -7,10 +7,10 @@ class DO{
         DO(uint8_t pin, uint32_t vref, uint32_t aref, int cal_mode);
         ~DO();
         void setTemperature(float temp);
-        float getDOValue();
         float getTemperature();
         float analogDO();
         float voltageDO();
+        float getDOValue();
         void outputDOData(int delay_time);
     private:
         uint32_t _vref, _aref;
