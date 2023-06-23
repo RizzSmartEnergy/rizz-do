7646249a12674fab9de039568b3cb0d4e090a8d3
Haidar Ali <haidarali.eu@gmail.com>
2023-06-23 12:46:17 +0700

    change objects

--- src/DO.h
+++ src/DO.h
@@ -6,15 +6,15 @@ class DO{
     public:
         DO(uint8_t pin, uint32_t vref, uint32_t aref, int cal_mode);
         ~DO();
-        void setTemperature(uint16_t temp);
-        int16_t getDOValue();
-        uint16_t getTemperature();
-        uint16_t analogDO();
+        void setTemperature(float temp);
+        float getDOValue();
+        float getTemperature();
+        float analogDO();
         float voltageDO();
         void outputDOData(int delay_time);
     private:
         uint32_t _vref, _aref;
-        uint16_t _temp;
+        float _temp;
         uint8_t _pin;
         int _cal_mode, _delay_time;
 };
