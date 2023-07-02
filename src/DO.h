d5dfe1e55ca593811651bcbf2b12250e92b1d943
Haidar Ali <haidarali.eu@gmail.com>
2023-07-02 16:14:49 +0700

    Add calibration function

--- src/DO.h
+++ src/DO.h
@@ -5,23 +5,30 @@
 class DO
 {
 public:
-    DO(uint8_t pin, double vref, double aref, int cal_mode);
+    DO(uint8_t pin, double vref, double aref);
     ~DO();
     void begin(int baudrate);
+    void characteristicDO();
+    boolean serialDataDO();
+    byte uartParsingDO();
+    void calibrationDO(byte mode);
     void setTemperature(float temp);
     float getTemperature();
     float getAnalogDO();
     float getVoltageDO();
     int getMedianDO(int bArray[], int iFilterLen);
-    float samplingDO();
+    float samplingVoltageDO();
+    float samplingTempDO();
     float getDO();
+    void run();
+    void modeDO();
     void getAllDOData(int delay_time);
 
 private:
     uint8_t _pin;
     double _vref, _aref;
     float _temp;
-    int _cal_mode, _baudrate, _delay_time;
+    int _baudrate, _delay_time;
 };
 
 #endif
