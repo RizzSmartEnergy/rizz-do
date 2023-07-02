74b1556bb07a506c0ec159b9f6421bed7624e01a
Haidar Ali <haidarali.eu@gmail.com>
2023-07-02 16:14:02 +0700

    Add Calibration Features

--- src/DO.cpp
+++ src/DO.cpp
@@ -1,26 +1,32 @@
 #include <Arduino.h>
+#include <pgmspace.h>
+#include <EEPROM.h>
 #include "DO.h"
 
-#define CAL1_V (131)
-#define CAL1_T (25)
-
-#define CAL2_V (1300)
-#define CAL2_T (15)
-
 #define SCOUNT 30
+
+#define EEPROM_write(address, p) {int i = 0; byte *pp = (byte*)&(p);for(; i < sizeof(p); i++) EEPROM.write(address+i, pp[i]);}
+#define EEPROM_read(address, p)  {int i = 0; byte *pp = (byte*)&(p);for(; i < sizeof(p); i++) pp[i]=EEPROM.read(address+i);}
+
+#define ReceivedBufferLength 20
+char receivedBuffer[ReceivedBufferLength+1];    // store the serial command
+byte receivedBufferIndex = 0;
 
 int analogBuffer[SCOUNT];
 int analogBufferTemp[SCOUNT];
 int analogBufferIndex = 0;
 int copyIndex = 0;
-float averageVoltage = 0;
 
-DO::DO(uint8_t pin, double vref, double aref, int cal_mode)
+#define SaturationDoVoltageAddress 12          //the address of the Saturation Oxygen voltage stored in the EEPROM
+#define SaturationDoTemperatureAddress 16      //the address of the Saturation Oxygen temperature stored in the EEPROM
+float SaturationDoVoltage,SaturationDoTemperature;
+float averageVoltage;
+
+DO::DO(uint8_t pin, double vref, double aref)
 {
     _pin = pin;
     _vref = vref;
     _aref = aref;
-    _cal_mode = cal_mode;
 }
 
 DO::~DO() {}
@@ -29,7 +35,9 @@ void DO::begin(int baudrate)
 {
     _baudrate = baudrate;
     Serial.begin(_baudrate);
+    EEPROM.begin(4095);
     pinMode(_pin, INPUT);
+    characteristicDO();
 }
 
 void DO::setTemperature(float temp)
@@ -42,6 +50,112 @@ float DO::getTemperature()
     return _temp;
 }
 
+void DO::characteristicDO(){
+    EEPROM_read(SaturationDoVoltageAddress, SaturationDoVoltage);
+    EEPROM_read(SaturationDoTemperatureAddress, SaturationDoTemperature);
+    if(EEPROM.read(SaturationDoVoltageAddress)==0xFF && EEPROM.read(SaturationDoVoltageAddress+1)==0xFF && EEPROM.read(SaturationDoVoltageAddress+2)==0xFF && EEPROM.read(SaturationDoVoltageAddress+3)==0xFF)
+    {
+      SaturationDoVoltage = 1127.6;//1127.6;   //default voltage:1127.6mv
+      EEPROM_write(SaturationDoVoltageAddress, SaturationDoVoltage);
+    }
+    if(EEPROM.read(SaturationDoTemperatureAddress)==0xFF && EEPROM.read(SaturationDoTemperatureAddress+1)==0xFF && EEPROM.read(SaturationDoTemperatureAddress+2)==0xFF && EEPROM.read(SaturationDoTemperatureAddress+3)==0xFF)
+    {
+      SaturationDoTemperature = 25.0;   //default temperature is 25^C
+      EEPROM_write(SaturationDoTemperatureAddress, SaturationDoTemperature);
+    }
+}
+
+boolean DO::serialDataDO(){
+  char receivedChar;
+  static unsigned long receivedTimeOut = millis();
+  while ( Serial.available() > 0 )
+  {
+    if (millis() - receivedTimeOut > 500U)
+    {
+      receivedBufferIndex = 0;
+      memset(receivedBuffer,0,(ReceivedBufferLength+1));
+    }
+    receivedTimeOut = millis();
+    receivedChar = Serial.read();
+    if (receivedChar == '\n' || receivedBufferIndex == ReceivedBufferLength)
+    {
+    receivedBufferIndex = 0;
+    strupr(receivedBuffer);
+    return true;
+    }else{
+        receivedBuffer[receivedBufferIndex] = receivedChar;
+        receivedBufferIndex++;
+    }
+  }
+  return false;
+}
+
+byte DO::uartParsingDO()
+{
+    byte modeIndex = 0;
+    if(strstr(receivedBuffer, "CALIBRATION") != NULL)
+        modeIndex = 1;
+    else if(strstr(receivedBuffer, "SATCAL") != NULL)
+        modeIndex = 2;
+    else if(strstr(receivedBuffer, "EXIT") != NULL)
+        modeIndex = 3;
+    return modeIndex;
+}
+
+
+void DO::calibrationDO(byte mode)
+{
+    char *receivedBufferPtr;
+    static boolean doCalibrationFinishFlag = 0,enterCalibrationFlag = 0;
+    float voltageValueStore;
+    switch(mode)
+    {
+      case 0:
+      if(enterCalibrationFlag)
+         Serial.println(F("Command Error"));
+      break;
+
+      case 1:
+      enterCalibrationFlag = 1;
+      doCalibrationFinishFlag = 0;
+      Serial.println();
+      Serial.println(F(">>>Enter Calibration Mode<<<"));
+      Serial.println(F(">>>Please put the probe into the saturation oxygen water! <<<"));
+      Serial.println();
+      break;
+
+     case 2:
+      if(enterCalibrationFlag)
+      {
+         Serial.println();
+         Serial.println(F(">>>Saturation Calibration Finish!<<<"));
+         Serial.println();
+         EEPROM_write(SaturationDoVoltageAddress, averageVoltage);
+         EEPROM_write(SaturationDoTemperatureAddress, _temp);
+         SaturationDoVoltage = averageVoltage;
+         SaturationDoTemperature = _temp;
+         doCalibrationFinishFlag = 1;
+      }
+      break;
+
+        case 3:
+        if(enterCalibrationFlag)
+        {
+            Serial.println();
+            if(doCalibrationFinishFlag)
+               Serial.print(F(">>>Calibration Successful"));
+            else
+              Serial.print(F(">>>Calibration Failed"));
+            Serial.println(F(",Exit Calibration Mode<<<"));
+            Serial.println();
+            doCalibrationFinishFlag = 0;
+            enterCalibrationFlag = 0;
+        }
+        break;
+    }
+}
+
+
 float DO::getAnalogDO()
 {
     return analogRead(_pin);
@@ -49,10 +163,10 @@ float DO::getAnalogDO()
 
 float DO::getVoltageDO()
 {
-    return (_vref * analogRead(_pin) / _aref);
+    return (getAnalogDO() * _vref / _aref);
 }
 
-static const float DO_Table[41] = {
+static const float DO_Table[41] PROGMEM= {
     14.460, 14.220, 13.820, 13.440, 13.090, 12.740, 12.420, 12.110, 11.810, 11.530,
     11.260, 11.010, 10.770, 10.530, 10.300, 10.080, 9.860, 9.660, 9.460, 9.270,
     9.080, 8.900, 8.730, 8.570, 8.410, 8.250, 8.110, 7.960, 7.820, 7.690,
@@ -82,7 +196,7 @@ int DO::getMedianDO(int bArray[], int iFilterLen)
     return bTemp;
 }
 
-float DO::samplingDO()
+float DO::samplingVoltageDO()
 {
     static unsigned long analogSampleTimepoint = millis();
     if (millis() - analogSampleTimepoint > 30U) // every 30 milliseconds,read the analog value from the ADC
@@ -93,7 +207,6 @@ float DO::samplingDO()
         if (analogBufferIndex == SCOUNT)
             analogBufferIndex = 0;
     }
-
     static unsigned long printTimepoint = millis();
     if (millis() - printTimepoint > 800U)
     {
@@ -101,17 +214,41 @@ float DO::samplingDO()
         for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++)
         {
             analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
-            averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * _vref / _aref;
+            averageVoltage = getMedianDO(analogBufferTemp, SCOUNT) * _vref / _aref;
         }
     }
     return averageVoltage;
 }
 
+float DO::samplingTempDO(){
+       static unsigned long tempSampleTimepoint = millis();
+   if(millis()-tempSampleTimepoint > 500U)  // every 500 milliseconds, read the temperature
+   {
+      //tempSampleTimepoint = millis();
+      _temp = 25.0;//readTemperature();  // add your temperature codes here to read the temperature, unit:^C
+   }
+
+}
+
 float DO::getDO()
 {
-    float saturationVoltage = (_cal_mode == 1) ? ((uint32_t)CAL1_V + (uint32_t)35 * _temp - (uint32_t)CAL1_T * 35) : ((int16_t)((int8_t)_temp - CAL2_T) * ((uint16_t)CAL1_V - CAL2_V) / ((uint8_t)CAL1_T - CAL2_T) + CAL2_V);
+    return (pgm_read_float_near( &DO_Table[0] + (int)(SaturationDoTemperature+0.5) ) * averageVoltage / SaturationDoVoltage);
+}
 
-    return ((samplingDO() * DO_Table[(int)_temp] / saturationVoltage));
+void DO::run(){
+    samplingVoltageDO();
+    samplingTempDO();
+}
+
+void DO::modeDO(){
+   if(serialDataDO() > 0)
+   {
+      byte modeIndex = 0;  //parse the uart command received
+      calibrationDO(modeIndex);    // If the correct calibration command is received, the calibration function should be called.
+   }
+
+   EEPROM.commit();
+
 }
 
 void DO::getAllDOData(int delay_time)
