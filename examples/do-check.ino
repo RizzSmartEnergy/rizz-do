464da50300210f2759561d65e860c51e331047b7
Haidar Ali <haidarali.eu@gmail.com>
2023-07-02 19:18:15 +0700

    add temp func

--- examples/do-check.ino
+++ examples/do-check.ino
@@ -1,17 +1,20 @@
 #include <Arduino.h>
-#include <pgmspace.h>
 #include <DO.h>
 
 #define PIN 34
+#define AREF 4095
 #define VREF 3300
-#define AREF 4095.0
 
 DO sensor(PIN, VREF, AREF);
 
 void setup(){
+  Serial.begin(115200);
   sensor.begin();
 }
 
 void loop(){
+  sensor.setTemperature(25.0);
   sensor.run();
+  sensor.getAllDOData();
+  delay(500);
 }
